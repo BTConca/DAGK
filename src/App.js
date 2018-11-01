f0c5dfdf846291892fd5ba6e6adc04f588567447
BTConca <1512250@student.fit.hcmus.vn>
2018-11-01 14:41:09 +0700

    Firt commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+
+var config = {
+  apiKey: "AIzaSyAzlLC0l-JJWNiSZ-Z-csObwbKLW_7gpsk",
+  authDomain: "chit-chat-18b09.firebaseapp.com",
+  databaseURL: "https://chit-chat-18b09.firebaseio.com",
+  projectId: "chit-chat-18b09",
+  storageBucket: "chit-chat-18b09.appspot.com",
+  messagingSenderId: "726573927821"
+};
+
+firebase.initializeApp(config);
 
 class App extends Component {
+  state = { isSignedIn: false }
+
+  uiConfig = {
+    signInFlow : "popup",
+    signInOptions :
+    [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID
+    ]
+    ,
+    callbacks:
+    {
+      signInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({isSignedIn : !!user})
+      if(user != null )
+      
+      console.log("user",user)
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.isSignedIn ?
+          (<div>
+            <div> Signed In!</div>
+            <button onClick={() => firebase.auth().signOut()}>Sign Out!</button>
+            <h1>Wellcome {firebase.auth().currentUser.displayName}</h1>
+            <img alt="profile picture" src={firebase.auth().currentUser.photoUrl} />
+          </div>)
+          :
+          (<div>
+            <StyledFirebaseAuth
+            uiConfig = {this.uiConfig}
+            firebaseAuth = {firebase.auth()}
+            />
+          </div>
+          )}
       </div>
     );
   }
